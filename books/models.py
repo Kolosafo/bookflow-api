0ba2f94b21be61012aa8700582eb8114a8c30b09
Dauda Kolo <88611380+Kolosafo@users.noreply.github.com>
2025-10-27 17:06:57 +0100

    fix(notes): added date created

--- books/models.py
+++ books/models.py
@@ -121,5 +121,6 @@ class Notes(models.Model):
     content = models.TextField(blank=True, null=True)
     title = models.CharField(max_length=255)
     book_author = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return f"{self.book_title}"
