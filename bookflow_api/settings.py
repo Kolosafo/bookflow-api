67849f21fcca4de84730b519b40839733ee8d9de
Dauda Kolo <88611380+Kolosafo@users.noreply.github.com>
2026-03-29 06:38:59 +0100

    Fix asgi settings and allowed hosts for heroku

--- bookflow_api/settings.py
+++ bookflow_api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = APP_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bookflow-api-547ff4a93bca.herokuapp.com/']
+ALLOWED_HOSTS = ['bookflow-api-547ff4a93bca.herokuapp.com']
 
 
 EMAIL_HOST = 'smtp.gmail.com'
